98939b04b7d6542cb23b6ee2dbede33d80c758d6
reka prihatanto <rey.barrolz@gmail.com>
2014-10-22 10:00:37 +0700

    rm envoy file

--- Envoy.blade.php
+++
@@ -1,7 +0,0 @@
-@servers(['web' => 'root@192.168.2.153'])
-
-@task('foo', ['on' => 'web'])
-	
-	echo User-Name={{ $username }},User-Password="{{ $pass }}"|/usr/bin/radclient -x  127.0.0.1 auth {{ $rad_secret_localhost }}
-
-@endtask
