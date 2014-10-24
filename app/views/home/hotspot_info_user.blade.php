b20293fafd8fdd1f6183e073d43a7ec9d245513f
reka prihatanto <rey.barrolz@gmail.com>
2014-10-24 10:28:30 +0700

    tambah keterangan nama di lvl user|home

--- app/views/home/hotspot_info_user.blade.php
+++ app/views/home/hotspot_info_user.blade.php
@@ -1,5 +1,12 @@
 <div class='col-md-6'>
 	<table class="table">
+<?php
+$p = Mst_Data_User::whereUsername(Auth::user()->username)->first();
+?>		
+			<tr>
+				<td>Nama</td>
+				<td width='10%'> <span class='label label-success'> @if(count($p)>0) {{  $p->nama }} @else - @endif</span> </td>
+			</tr>		
 			<tr>
 				<td>User Name</td>
 				<td width='10%'> <span class='label label-success'>{{  Auth::user()->username }}</span> </td>
