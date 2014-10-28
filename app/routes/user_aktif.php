3128dc5013da5ac8e87dd08fab14bca6f8713e70
reka prihatanto <rey.barrolz@gmail.com>
2014-10-28 08:30:30 +0700

    .

--- app/routes/user_aktif.php
+++ app/routes/user_aktif.php
@@ -3,3 +3,5 @@
 Route::get('user_aktif', ['before' => 'auth|admin', 'uses' => 'UseraktifController@index']);
 Route::post('kick_user', ['before' => 'auth|admin', 'uses' => 'UseraktifController@kick_user']);
 Route::post('update_time_refresh', ['as' => 'user_aktif_update_time_refresh', 'before' => 'auth|admin', 'uses' => 'UseraktifController@update_time_refresh']);
+Route::post('kick_all_user', ['as' => 'kick_all_user',  'before' => 'auth|admin', 'uses' => 'UseraktifController@kick_all_user']);
+
