424f25719f1f238a2b234a4814d52b97ba1fbc74
reka prihatanto <rey.barrolz@gmail.com>
2014-11-06 11:47:16 +0700

    edit template post admin

--- app/views/user_hotspot/popup/view_detail.blade.php
+++ app/views/user_hotspot/popup/view_detail.blade.php
@@ -1,17 +1,21 @@
-<h3>Detail User : {{ $d->username }}</h3>
-<hr>
+@if(count($d)>0)
+	<h3>Detail User : {{ $d->username }}</h3>
+	<hr>
 
-<table class="table table-hover">
- 		<tr>
-			<td width='40px'>Username</td>
-			<td>{{ $d->username }}</td>
-		</tr>
- 		<tr>
-			<td>Password</td>
-			<td>{{ $d->value }}</td>
-		</tr>
- 		<tr>
-			<td></td>
-			<td></td>
-		</tr>
- </table>
+	<table class="table table-hover">
+	 		<tr>
+				<td width='40px'>Username</td>
+				<td>{{ $d->username }}</td>
+			</tr>
+	 		<tr>
+				<td>Password</td>
+				<td>{{ $d->value }}</td>
+			</tr>
+	 		<tr>
+				<td></td>
+				<td></td>
+			</tr>
+	 </table>
+ @else
+	 detail tdk ditemukan
+ @endif
