1231a2de0f8457f39a2424aa0557871f4ac8ed18
reka prihatanto <rey.barrolz@gmail.com>
2014-12-13 06:55:11 +0700

    edit template popup atribut menu user 1

--- app/views/user_hotspot/popup/view_attributes.blade.php
+++ app/views/user_hotspot/popup/view_attributes.blade.php
@@ -1,4 +1,5 @@
 Reply Attributes
+<hr style='margin:3px'>
 <button class='btn btn-success pull-right' id='add'> <i class='fa fa-plus-square'></i> create</button>
 <script type="text/javascript">
 $('#add').click(function(){
