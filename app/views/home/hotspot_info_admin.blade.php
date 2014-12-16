468e0a4a7bac2fc44d0e8443ef6a033d984e2e0f
reka prihatanto <rey.barrolz@gmail.com>
2014-12-16 08:48:21 +0700

    .

--- app/views/home/hotspot_info_admin.blade.php
+++ app/views/home/hotspot_info_admin.blade.php
@@ -2,12 +2,12 @@
 	<table class="table">
 			<tr>
 				<td>User Hotspot</td>
-				<td width='10%'> <span class='label label-success'> </span> </td>
+				<td width='10%'> <span class='label label-success'>{{ Radius_Radcheck::count() }}</span> </td>
 			</tr>
 
 			<tr>
 				<td>User Online</td>
-				<td width='10%'> <span class='label label-success'> </span> </td>
+				<td width='10%'> <span class='label label-success'>{{ Radius_Radacct::where('acctstoptime', '=', NULL)->count() }}</span> </td>
 			</tr>
 
 			<tr>
